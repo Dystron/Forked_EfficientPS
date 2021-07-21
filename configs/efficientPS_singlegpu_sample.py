c0a178eaa63f9f22890d7604e2112d19ca241904
Yannick Vogt <yannick.vogt@gmail.com>
2021-07-21 09:38:13 +0200

    adapted singleGpu settings to be as close as possible to b0.py

--- configs/efficientPS_singlegpu_sample.py
+++ configs/efficientPS_singlegpu_sample.py
@@ -3,7 +3,7 @@ model = dict(
     type='EfficientPS',
     pretrained=True,
     backbone=dict(
-        type='tf_efficientnet_b5',
+        type='tf_efficientnet_b0',
         act_cfg = dict(type="Identity"),
         norm_cfg=dict(type='InPlaceABN', activation='leaky_relu', activation_param=0.01, requires_grad=True),
         style='pytorch'),
@@ -137,9 +137,9 @@ img_norm_cfg = dict(
 train_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(type='LoadAnnotations', with_bbox=True, with_mask=True, with_seg=True),
-    dict(type='Resize', img_scale=(512, 256), ratio_range=(0.5, 2.0), keep_ratio=True),
+    dict(type='Resize', img_scale=(1024, 512), ratio_range=(0.5, 2.0), keep_ratio=True),
     dict(type='RandomFlip', flip_ratio=0.5),
-    dict(type='RandomCrop', crop_size=(128, 256)),
+    dict(type='RandomCrop', crop_size=(256, 512)),
     dict(type='Normalize', **img_norm_cfg),
     dict(type='Pad', size_divisor=32),
     dict(type='DefaultFormatBundle'),
@@ -162,8 +162,8 @@ test_pipeline = [
 ]
 
 data = dict(
-    imgs_per_gpu=2,
-    workers_per_gpu=2,
+    imgs_per_gpu=4,
+    workers_per_gpu=4,
     train=dict(
         type=dataset_type,
         ann_file=data_root + 'annotations/train.json',
@@ -186,15 +186,15 @@ data = dict(
         pipeline=test_pipeline))
 evaluation = dict(interval=1, metric=['panoptic'])
 # optimizer
-optimizer = dict(type='SGD', lr=0.07, momentum=0.9, weight_decay=0.0001)
-optimizer_config = dict(grad_clip=dict(max_norm=35, norm_type=2))
+optimizer = dict(type='SGD', lr=0.08, momentum=0.9, weight_decay=0.0001)
+optimizer_config = dict(grad_clip=None) #dict(max_norm=35, norm_type=2))
 # learning policy
 lr_config = dict(
     policy='step',
     warmup='linear',
     warmup_iters=500,
     warmup_ratio=1.0 / 3,
-    step=[120, 144])
+    step=[90, 140])
 checkpoint_config = dict(interval=1)
 # yapf:disable
 log_config = dict(
